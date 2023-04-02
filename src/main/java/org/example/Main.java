04ddde1dc53399b964fb5594857145f334c30078
zhannatsh <tsjanie@gmail.com>
2023-04-02 20:16:01 +0600

    New files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodShowMessageAble();
+        mainMethodShowInfo();
+    }
+    private static void mainMethodShowMessageAble() {
+        System.out.println("Hello able QA course people\n");
+    }
+    private static void mainMethodShowInfo() {
+        System.out.println("New changes");
+    }
+}
